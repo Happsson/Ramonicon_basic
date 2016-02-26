a237cec8de39ffd989edd50c907bfe60abdc7f95
Hannes Paulsson <hannespa@kth.se>
2016-02-26 20:07:55 +0100

    DONE

--- app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/AudioHandler.java
+++ app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/AudioHandler.java
@@ -14,44 +14,5 @@ public class AudioHandler {
 
 
 
-    public static void getRandomSound(Context activity, int numberOfPlayers,MediaPlayer sound){
-
-        Random random = new Random();
-        int soundGetter = random.nextInt(numberOfPlayers);
-        getSound(activity, soundGetter, sound);
-
-    }
-
-    public static void getSound(Context activity, int choice, MediaPlayer sound) {
-
-        switch (choice) {
-            case 0:
-
-                sound.create(activity, R.raw.sound0);
-                sound.start();
-                break;
-            case 1:
-                sound.create(activity, R.raw.sound1);
-                sound.start();
-                break;
-
-            case 2:
-                sound = new MediaPlayer().create(activity, R.raw.sound2);
-                sound.start();
-                break;
-
-            case 3:
-                sound = new MediaPlayer().create(activity, R.raw.sound3);
-                sound.start();
-                break;
-
-            case 4:
-                sound = new MediaPlayer().create(activity, R.raw.sound4);
-                sound.start();
-                break;
-
-        }
-    }
-
 
 }
