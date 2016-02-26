250563f9fd87e36581c49e257c5bdd72a966d26f
Hannes Paulsson <hannespa@kth.se>
2016-02-26 19:52:51 +0100

    fix

--- app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/AudioHandler.java
+++ app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/AudioHandler.java
@@ -1,6 +1,7 @@
 package nu.geeks.ramonicon.ramonicon_basic;
 
 import android.app.Activity;
+import android.content.Context;
 import android.media.MediaPlayer;
 
 import java.util.ArrayList;
@@ -10,54 +11,44 @@ import java.util.Random;
  * Created by Micke on 2016-02-26.
  */
 public class AudioHandler {
-    private static int numberOfPlayers;
-
-    Activity activity;
 
 
-    public AudioHandler(Activity activity , int numberOfPlayers) {
-        this.numberOfPlayers = numberOfPlayers;
-        this.activity = activity;
 
-    }
+    public static void getRandomSound(Context activity, int numberOfPlayers,MediaPlayer sound){
 
-    public static int getNumberOfPlayers() {
-        return numberOfPlayers;
-    }
-
-    public static void setNumberOfPlayers(int numberOfPlayers) {
-        AudioHandler.numberOfPlayers = numberOfPlayers;
-    }
-
-    public void getRandomSound(){
-        MediaPlayer sound = new MediaPlayer();
         Random random = new Random();
         int soundGetter = random.nextInt(numberOfPlayers);
-        sound = getSound(sound,soundGetter);
-        sound.start();
+        getSound(activity, soundGetter, sound);
+
     }
 
-    public MediaPlayer getSound(MediaPlayer sound,int choice) {
+    public static void getSound(Context activity, int choice, MediaPlayer sound) {
 
         switch (choice) {
             case 0:
-            sound.create(activity, R.raw.sound0);
-                return sound;
+
+                sound.create(activity, R.raw.sound0);
+                sound.start();
+                break;
             case 1:
                 sound.create(activity, R.raw.sound1);
-                return sound;
+                sound.start();
+                break;
 
             case 2:
-                sound.create(activity, R.raw.sound2);
-                return sound;
+                sound = new MediaPlayer().create(activity, R.raw.sound2);
+                sound.start();
+                break;
 
             case 3:
-                sound.create(activity, R.raw.sound3);
-                return sound;
+                sound = new MediaPlayer().create(activity, R.raw.sound3);
+                sound.start();
+                break;
 
-            default:
-                sound.create(activity, R.raw.sound4);
-                return sound;
+            case 4:
+                sound = new MediaPlayer().create(activity, R.raw.sound4);
+                sound.start();
+                break;
 
         }
     }
--- app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/MainActivity.java
+++ app/src/main/java/nu/geeks/ramonicon/ramonicon_basic/MainActivity.java
@@ -1,13 +1,89 @@
 package nu.geeks.ramonicon.ramonicon_basic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    AudioHandler audioHandler;
+    Button b1, b2, b3, b4, b5, start;
+    EditText numberPlayer;
+    MediaPlayer mPLayer;
+    int numbPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPLayer = new MediaPlayer();
+
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+
+        start = (Button) findViewById(R.id.bStarta);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        start.setOnClickListener(this);
+
+        numberPlayer = (EditText) findViewById(R.id.edNum);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        MediaPlayer mediaPlayer;
+        switch(v.getId()){
+            case R.id.b1:
+               //  audioHandler.getSound(mPLayer, 0);
+                mediaPlayer = MediaPlayer.create(this, R.raw.sound0);
+                mediaPlayer.start();
+                break;
+            case R.id.b2:
+                mediaPlayer = MediaPlayer.create(this, R.raw.sound1);
+                mediaPlayer.start();
+
+                break;
+            case R.id.b3:
+                mediaPlayer = MediaPlayer.create(this, R.raw.sound2);
+                mediaPlayer.start();
+
+                break;
+            case R.id.b4:
+                mediaPlayer = MediaPlayer.create(this, R.raw.sound3);
+                mediaPlayer.start();
+
+                break;
+            case R.id.b5:
+                mediaPlayer = MediaPlayer.create(this, R.raw.sound4);
+                mediaPlayer.start();
+
+                break;
+            case R.id.bStarta:
+                Intent intent = new Intent(this, PlayActivity.class);
+                numbPlayer = Integer.parseInt(numberPlayer.getText().toString());
+                if(numbPlayer < 1 || numbPlayer > 5){
+                    Toast.makeText(this, "Välj mellan en och fem spelare", Toast.LENGTH_LONG).show();
+                }else{
+                    intent.putExtra("players", numbPlayer);
+                    startActivity(intent);
+                }
+                break;
+
+        }
     }
 }
